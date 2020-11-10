3a86dbe075dd4590c7c469d419d21397df7fceb6
drmats <drmats@users.noreply.github.com>
2020-11-10 20:27:01 +0100

    Test script.

---
+++ scripts/try_sending_email.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#
+# Cloudmailer.
+#
+# @license BSD-2-Clause
+# @copyright Mat. 2020
+#
+
+curl \
+    -X POST \
+    -H "Content-Type: application/json" \
+    -d '{"replyTo": "cloudmailer@mailinator.com", "subject": "Hi!", "text": "Good. ;)"}' \
+    localhost:8080/cloudmailer/api/v1/send/ | \
+    python -m json.tool
